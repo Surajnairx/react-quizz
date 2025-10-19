56f19a5f38ebeef31ec5134bd2e735424a24bd17
Suraj Nair <surajnair9698@gmail.com>
2025-10-19 21:14:58 +0530

    feat: shuffling answers and providing a quiz completed prompt

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import completedImage from "../assets/quiz-complete.png";
 
 function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
   const activeQuestionIndex = userAnswer.length;
+
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectAnswer) {
     setUserAnswer((prevAns) => {
       return [...prevAns, selectAnswer];
     });
   }
+  if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={completedImage} alt="" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+  const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswer.sort((a, b) => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="question">
         <h2>Question : {QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswer.map((answer, index) => (
             <li className="answer" key={index}>
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
