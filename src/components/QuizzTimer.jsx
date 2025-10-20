1ab851b1406688f21ad05ca2ffe97d0fd61c8e81
Suraj Nair <surajnair9698@gmail.com>
2025-10-20 14:30:48 +0530

    feat: setting timers for every question - stage 1

---
+++ src/components/QuizzTimer.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function QuizzTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timeoutVariable = setTimeout(onTimeout, timeout);
+    console.log("SETTING TIMEOUT");
+    return () => {
+      clearTimeout(timeoutVariable);
+    };
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    console.log("SETTING INTERVAL");
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10, 100);
+    });
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" value={remainingTime} max={timeout} />;
+}
+
+export default QuizzTimer;
