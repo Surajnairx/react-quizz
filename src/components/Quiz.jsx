deb63f54c45b0db1fe625a0cdc91bdb55936e3fc
Suraj Nair <surajnair9698@gmail.com>
2025-10-21 16:06:39 +0530

    feat: styling the correct and incorrect answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,18 +1,40 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import QUESTIONS from "../questions";
 import completedImage from "../assets/quiz-complete.png";
+import QuizzTimer from "./QuizzTimer";
 
 function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswer, setUserAnswer] = useState([]);
-  const activeQuestionIndex = userAnswer.length;
+  const activeQuestionIndex =
+    answerState === "" ? userAnswer.length : userAnswer.length - 1;
 
   const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectAnswer) {
-    setUserAnswer((prevAns) => {
-      return [...prevAns, selectAnswer];
-    });
-  }
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectAnswer) {
+      setAnswerState("answered");
+      setUserAnswer((prevAns) => {
+        return [...prevAns, selectAnswer];
+      });
+      setTimeout(() => {
+        if (selectAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+      }, 1000);
+      setTimeout(() => {
+        setAnswerState("");
+      }, 2000);
+    },
+    [activeQuestionIndex]
+  );
+
+  const handleSkipAnswer = useCallback(() => {
+    handleSelectAnswer(null);
+  }, [handleSelectAnswer]);
+
   if (quizIsCompleted) {
     return (
       <div id="summary">
@@ -23,18 +45,40 @@ function Quiz() {
   }
   const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
   shuffledAnswer.sort((a, b) => Math.random() - 0.5);
+
   return (
     <div id="quiz">
+      <QuizzTimer
+        key={activeQuestionIndex}
+        timeout={10000}
+        onTimeout={handleSkipAnswer}
+      />
       <div id="question">
         <h2>Question : {QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffledAnswer.map((answer, index) => (
-            <li className="answer" key={index}>
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledAnswer.map((answer, index) => {
+            const isSelected = userAnswer[userAnswer.length - 1] === answer;
+            let cssClasses = "";
+            if (answerState === "answered" && isSelected) {
+              cssClasses = "selected";
+            }
+            if (
+              (answerState === "correct" || answerState === "wrong") &&
+              isSelected
+            ) {
+              cssClasses = answerState;
+            }
+            return (
+              <li className="answer" key={index}>
+                <button
+                  className={cssClasses}
+                  onClick={() => handleSelectAnswer(answer)}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
